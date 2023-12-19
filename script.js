99a65951d90a54de707b212f57457aba3ac60180
unknown <thomaspg@live.co.uk>
2023-12-19 18:31:06 +0000

    add a bit of CSS, fix multi search bug

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-console.log("hi")
 
 // Event declarations
 
+
+// For API Key
 const apiKey = "7867288bff076c11ab0fe1c5a52166df"
 
+// For form
 const inputEl = document.getElementById('search-input');
 const submitEL = document.getElementById('search-button');
+
+// For later use 
 let userSearch;
 let queryURL;
 
+// For weather card
 const cardCityName = document.getElementById('card-city-name');
 const cardDate = document.getElementById('card-date')
+const cardTemp = document.getElementById('temp');
+const cardWind = document.getElementById('wind');
+const cardHumidity = document.getElementById('humidity');
 
-// Event Listener that grabs user input and 
+// For the weather boxes
+const boxContainer = document.getElementById('weather-boxes-container')
+
+// Event Listener that grabs user input and feeds it into fetch request to get data back into the HTML
 
 submitEL.addEventListener('click', (e) => {
     e.preventDefault();
@@ -40,11 +51,47 @@ function fetchData() {
     })
     .then(function (data) {
         console.log(data);
-        console.log(data.city.name)
-        console.log(data.list[0].dt_txt);
+
+
+
         // Appending to the weather card
         cardCityName.textContent = data.city.name;
         cardDate.textContent = data.list[0].dt_txt;
+        cardTemp.textContent = 'Temp: ' + data.list[0].main.temp + '°C';
+        cardWind.textContent = 'Windspeed: ' + data.list[0].wind.speed + 'k/h';
+        cardHumidity.textContent = 'Humidity ' + data.list[0].main.humidity;
+
+        // Clearing any previous searches' boxes
+        while (boxContainer.firstChild) {
+            boxContainer.removeChild(boxContainer.lastChild);
+          }
+
+        // Appending to all boxes
+        for (i = 0; i < data.list.length; i++) {
+            let weatherBox = document.createElement('div')
+            
+            let boxDate = document.createElement('p');
+            boxDate.textContent = data.list[i].dt_txt;
+            weatherBox.appendChild(boxDate);
+
+            let boxIcon = document.createElement('img')
+            boxIcon.src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
+            weatherBox.appendChild(boxIcon);
+
+            let boxTemp = document.createElement('p');
+            boxTemp.textContent = 'Temp: ' + data.list[i].main.temp + '°C'
+            weatherBox.appendChild(boxTemp);
+
+            let boxWind = document.createElement('p');
+            boxWind.textContent = 'Windspeed: ' + data.list[i].wind.speed + 'k/h';
+            weatherBox.appendChild(boxWind);
+
+            let boxHumidity = document.createElement('p');
+            boxHumidity.textContent = 'Humidity ' + data.list[i].main.humidity;
+            weatherBox.appendChild(boxHumidity);
+
+            boxContainer.appendChild(weatherBox);
+        }
     })
 }
 
